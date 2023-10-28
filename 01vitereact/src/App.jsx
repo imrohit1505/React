bd258a990b36aab10869ad6434d1bf0d03aeb953
Rohit Ranjan <rohitranjan@Rohits-MacBook-Air.local>
2023-10-28 22:29:57 +0530

    created component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,13 @@
-
+import Chai from "./Chai"
 
 function App() {
 
   return (
-    <h1>Chai aur react with vite | Rohit Ranjan.</h1>
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
