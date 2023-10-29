e219721ea6453b2aa1d74da70f8e82cd47e795de
Rohit Ranjan <rohitranjan@Rohits-MacBook-Air.local>
2023-10-29 22:25:04 +0530

    created a new project to learn hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    if(counter < 20){
+      setCounter(counter+1)
+    }
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter-1)
+    }
+  }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h3>Counter value: {counter}</h3>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br/>
+      <button onClick={removeValue}>Remove value {counter}</button>
+    </>
+  )
+}
+
+export default App
